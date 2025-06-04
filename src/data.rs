e366772719186d23481c74f522baa06e621d6c20
TheAlexC <thealexc87@gmail.com>
2025-06-04 17:41:06 -0400

    add hurt and heal functions

--- src/data.rs
+++ src/data.rs
@@ -3,6 +3,8 @@
 
 // Todo: Honglos Hüberhaus
 
+use std::collections::HashMap;
+
 pub struct Character {
     level: u8,
 
@@ -17,14 +19,11 @@ pub struct Character {
 
     saves: Saves,
 
-    max_hp: u16,
     damage: u16,
     temp_hp: u16,
 
-    conditions: Vec<Condition>,
-    resistances: Vec<String>,
-    immunities: Vec<String>,
-    vulnerabilities: Vec<String>,
+    conditions: HashMap<String, u8>,
+    damage_types: HashMap<String, DamageScale>,
 
     skills: Skills,
 
@@ -37,10 +36,86 @@ pub struct Character {
     wealth: Wealth,
 }
 
+impl Character {
+    fn max_hp(&self) -> u16 {
+        return self.ancestry.base_hp as u16 + (self.level as u16)*(self.class.hp as u16 + self.attributes.constitution.bonus as u16);
+    }
+
+    fn hurt(&mut self, dmg_amount: u16, dmg_type: String, crit: bool) {
+        
+        let prescaler: f32;
+        
+        if let Some(dmg_scale) = self.damage_types.get(&dmg_type) {
+            prescaler = match dmg_scale {
+                DamageScale::Vulnerable => 2.0,
+                DamageScale::Resistant => 0.5,
+                DamageScale::Immune => 0.0,
+            }
+        } else {
+            prescaler = 1.0;
+        }
+
+        let total_damage = (prescaler * dmg_amount as f32).floor() as u16;
+
+        if total_damage == 0 {return};
+
+        if let Some(dying) = self.conditions.get("Dying") {
+            if crit {
+                self.conditions.insert(String::from("Dying"), dying + 2);
+            } else {
+                self.conditions.insert(String::from("Dying"), dying + 1);
+            }
+
+            let doomed = self.conditions.get("Doomed").unwrap_or(&0_u8);
+            let wounded = self.conditions.get("Wounded").unwrap_or(&0_u8);
+
+            if self.conditions["Dying"] >= (4 - doomed - wounded) {
+                self.conditions.insert(String::from("Dying"), 0);
+                self.conditions.insert(String::from("Dead"), 1);
+                self.conditions.insert(String::from("Doomed"), 0);
+            }
+        } else {
+            if total_damage <= self.temp_hp {
+                self.temp_hp -= total_damage;
+            } else if total_damage < self.max_hp() + self.damage {
+                self.temp_hp = 0;
+                self.damage += total_damage;
+            } else {
+                self.damage = self.max_hp();
+                self.conditions.insert(String::from("Dying"), 1);
+            }
+        }
+
+    }
+
+    fn heal(&mut self, heal_amount: u16){
+                
+        if heal_amount <= self.damage {
+            self.damage -= heal_amount;
+        } else {
+            self.damage = 0;
+        }
+
+        if let Some(_) = self.conditions.get("Dying") {
+            self.conditions.insert(String::from("Dying"), 0);
+
+            let wounded = self.conditions.get("Wounded").unwrap_or(&0_u8);
+
+            self.conditions.insert(String::from("Wounded"), wounded + 1);
+        }
+    }
+}
+
+enum DamageScale {
+    Vulnerable, 
+    Resistant,
+    Immune,
+}
+
 struct Class {
     id: Identifier,
     key_attribute: Attribute,
-    base_hp: u8,
+    hp: u8,
     saves: Saves,
     skills: Vec<(Skill, Proficiency)>,
     bonus_skills: u8,
@@ -110,12 +185,12 @@ enum Skill {
 }
 
 struct Attributes {
-    str: AttributeValue,
-    dex: AttributeValue,
-    con: AttributeValue,
-    int: AttributeValue,
-    wis: AttributeValue,
-    cha: AttributeValue,
+    strength: AttributeValue,
+    dexterity: AttributeValue,
+    constitution: AttributeValue,
+    intelligence: AttributeValue,
+    wisdom: AttributeValue,
+    charisma: AttributeValue,
 }
 
 struct Saves {
